cde7ab3052759e9245b2dd1e0a0fe1247571e5f6
Naomi Earle <naomiearle@Markbook-Bro.local>
2023-01-13 10:32:50 +0000

    setting up props for FeatureAbstract component

---
+++ src/components/FeatureAbstract.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="feature-abstract">
+    <h1>{{ msg }}</h1>
+    <img :src="require(`../assets/${imgName}`)" alt="Something">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureAbstract',
+  props: {
+    msg: String,
+    imgName: String
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <Jumbotron />
+    <FeatureAbstract msg="Hello!" imgName="placeholder-screenshot.png"/>
   </div>
 </template>
 
 <script>
 import TopBanner from '@/components/TopBanner.vue'
 import Jumbotron from '@/components/Jumbotron.vue'
+import FeatureAbstract from '@/components/FeatureAbstract.vue'
 
 export default {
   name: 'HomeView',
   components: {
     TopBanner,
-    Jumbotron
+    Jumbotron,
+    FeatureAbstract
   }
 }
 </script>
